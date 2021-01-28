5fa442d6b476021f67c4b56269ffd334a8d96980
Matthew Herod <matthew.herod@myunidays.com>
2021-01-28 09:02:16 +0000

    [automated] Update Formula/resharkercli.rb for resharkercli-0.0.2.big_sur.bottle.tar

--- Formula/resharkercli.rb
+++ Formula/resharkercli.rb
@@ -1,17 +1,16 @@
 class Resharkercli < Formula
+  VERSION = "0.0.3"
   desc "A combined Git and Jira CLI written for Kotlin/Native and JVM 🦈"
   homepage ""
-  version "0.0.2"
-  url "https://github.com/mherod/resharkercli/archive/0.0.2.tar.gz"
-  sha256 "5f75508af012a6bde56c5adb4ba1fe238a684d560404386c64649d9da142ffb0"
+  version VERSION
+  url "https://github.com/mherod/resharkercli/archive/#{VERSION}.tar.gz"
   license ""
   head "https://github.com/mherod/resharkercli.git"
 
   bottle do
-    root_url "https://github.com/mherod/resharkercli/releases/download/0.0.2"
+    root_url "https://github.com/mherod/resharkercli/releases/download/0.0.3"
     cellar :any_skip_relocation
-    sha256 "b3764cf7d4566a0a51a170a4518c38fc3c511bb36f32b2b7afc1245232e02154" => :big_sur
-    sha256 "5063c2d0c02677cf75d9e39d2bd63e377c0a05f1f33432ed61a4b58386c0deb1" => :catalina
+    sha256 "e55a696354d8b4dd1eb9806327d3fefb9f07221b275bbbc44cc063838e08441c" => :big_sur
   end
 
   depends_on :xcode => ["12.0", :build]
