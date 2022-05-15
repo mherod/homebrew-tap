4923aa2b2055bf55511e690100ee14e40c36d0ab
Matthew Herod <matthew.herod@myunidays.com>
2022-05-15 06:57:57 +0100

    Create get-cookie.rb

---
+++ Formula/get-cookie.rb
@@ -0,0 +1,33 @@
+class GetCookie < Formula
+  VERSION = "0.0.1"
+  desc "Node.js module for querying a local user's Chrome cookie"
+  homepage ""
+  version VERSION
+  url "https://github.com/mherod/get-cookie/archive/#{VERSION}.tar.gz"
+  license ""
+  head "https://github.com/mherod/get-cookie.git"
+
+  bottle do
+    root_url "https://github.com/mherod/get-cookie/releases/download/0.0.1"
+    cellar :any_skip_relocation
+  end
+
+  depends_on :xcode => ["12.0", :build]
+
+  def install
+    system "npm", "install""
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test resharkercli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
